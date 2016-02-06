5e740d11165b6463b68d985f3aabd0023e15ee4a
David Pan <codingisfun26@gmail.com>
2016-02-05 18:26:04 -0800

    added adminuser created to seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131090205) do
+ActiveRecord::Schema.define(version: 20160131015114) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
